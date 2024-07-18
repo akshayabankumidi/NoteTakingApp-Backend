017fe27089083ef2cfd72b7ccbbfd4eb70948918
akshayabankumidi <akshayabankumidi@gmail.com>
2024-07-18 14:31:05 +0530

    created User and Note models

---
+++ models/notes.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: [{ type: String }],
+  backgroundColor: { type: String, default: 'white' },
+  isArchived: { type: Boolean, default: false },
+  isDeleted: { type: Boolean, default: false },
+  deletedAt: { type: Date },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Note', NoteSchema);
